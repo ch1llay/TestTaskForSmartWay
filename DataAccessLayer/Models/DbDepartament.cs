e6c6554c4628724fb35a64eeb530e955627cdcf8
ch1llay <ch1llay@yandex.ru>
2022-03-14 01:43:36 +0300

    fix

--- DataAccessLayer/Models/DbDepartament.cs
+++ DataAccessLayer/Models/DbDepartament.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace DataAccessLayer.Models
 {
     public class DbDepartament
     {
+        [Key]
         public string Name { get; set; }
         public string Phone { get; set; }
     }
